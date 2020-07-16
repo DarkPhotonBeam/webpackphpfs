23c548b1ba5a6aacc5d894e3455feec1dd01e6af
Dan Photon <danphoton@yahoo.com>
2020-07-16 15:37:46 +0200

    Production Commit

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ Remove-Item './production/*' -Recurse -Force -Confirm:$false
 npm run build
 Copy-Item -Path "C:\xampp\htdocs\roots\webpackphpfullstack\*" -Destination './production' -Recurse
 #Copy-Item -Path (Get-Item -Path "C:\xampp\htdocs\roots\webpackphpfullstack\*" -Exclude ('node_modules')).FullName -Destination '../../dist' -Recurse -Force
-Copy-Item -Path "./dist" -Destination './production' -Recurse
+Copy-Item -Path "./dist/*" -Destination './production' -Recurse
 #Set-Location './production'
 #yarn install --production=true
 git add .
